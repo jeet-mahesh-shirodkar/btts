7ffc5d3580787cfd44e20c314b921da27d1e3651
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2020-05-03 13:12:57 +0530

    initial

---
+++ src/Components/Header/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return(
+   <div className="myNav">
+    <div className="Nav">
+      <ul>
+        <li>Home</li>
+        <li>Vehicle Rental</li>
+        <li>Activities</li>
+        <li>About</li>
+      </ul>
+    </div>
+  </div>
+    );
+}
+
+export default Header;
+
