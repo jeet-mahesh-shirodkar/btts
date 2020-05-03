58403e2d718006955347e3873bb1d2cc0a2f57b9
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2020-05-03 23:58:38 +0530

    Information Bar

--- src/Components/Beach/beach.js
+++ src/Components/Beach/beach.js
@@ -7,11 +7,22 @@ import './beach.css';
 export const BeachComp = ()=>{
     return(
     <div className="beach">
-    <img src={Beach} alt="bg"></img>
-    <img src={Sun} alt="sun"></img>
+    <img src={Beach} alt="bg"/>
+    <img src={Sun} alt="sun"/>
     <div className="care">
         <p>We Care We Plan</p>
         <Button variant="light">Contact US</Button>
+    </div>
+    <div className="info">
+        <div className="info1">
+                <h3>Vehicle Rental</h3>
+                <h5>Car Hire</h5>
+                <p>SELF DRIVING</p>
+                <p>DRIVER DRIVING</p>
+                <h5>Bike Hire</h5>   
+        </div>
+        <div className="info2"></div>
+        <div className="info3"></div>
     </div>
     </div>
     );
