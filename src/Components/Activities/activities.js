8087e9415b331bc6df72f755d231a712c60020a8
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2020-06-17 21:51:01 +0530

    Water Sports

--- src/Components/Activities/activities.js
+++ src/Components/Activities/activities.js
@@ -6,6 +6,10 @@ import bananaBoat from '../../Images/Activities/bananagBoat.jpg';
 import boat from '../../Images/Activities/bumperBoat.jpg';
 import jet from '../../Images/Activities/jetSkiing.jpg';
 import Card from 'react-bootstrap/Card';
+import snorkel from '../../Images/Activities/snorkel-hierro.jpg';
+import dolphin from '../../Images/Activities/dolphin.jpg';
+import banglow from '../../Images/Activities/Banglow.jpg';
+import scuba from '../../Images/Activities/scuba.jpg';
 import './activities.css';
 import { Container,Row, Col} from 'react-bootstrap';
 
@@ -13,9 +17,6 @@ export const Activities = () => {
         return (
         <React.Fragment>
             <div className="head">Water Sports</div>
-                {/* <div className="preHead">
-                    <p>family re-connecting the pixels</p>
-                </div> */}
                <Container>
                     <hr/>    
                    <Row>
@@ -59,17 +60,68 @@ export const Activities = () => {
                             <Card className="bg-dark">
                                 <Card.Img src={jet} alt="jet" />
                                     <Card.ImgOverlay>
-                                        <Card.Title><h3>Scurf</h3></Card.Title>
+                                        <Card.Title><h3>Jet Ski Ride</h3></Card.Title>
                                     </Card.ImgOverlay>
                             </Card>
                         </Col>
                         <Col md={2}></Col>
                     </Row>
                     <hr/>
-                </Container>   
-            {/* <div className="activitiesMain">
-                <img src={Main} alt="activityMain"/>
-            </div> */}
+                </Container> 
+                <div className="head">Island Trip</div>
+                <Container>
+                    <hr/>    
+                   <Row>
+                       <Col md={4}>
+                            <Card className="bg-dark">
+                                <Card.Img src={snorkel} alt="snorkel" />
+                                    <Card.ImgOverlay>
+                                        <Card.Title><h3>Snorkelling</h3></Card.Title>
+                                    </Card.ImgOverlay>
+                            </Card>
+                       </Col>
+                       <Col md={4}>
+                            <Card className="bg-dark">
+                                <Card.Img src={dolphin} alt="dolphin" />
+                                    <Card.ImgOverlay>
+                                        <Card.Title><h3>Dolphin</h3></Card.Title>
+                                    </Card.ImgOverlay>
+                            </Card>
+                        </Col>
+                        <Col md={4}>
+                            <Card className="bg-dark">
+                                <Card.Img src={banglow} alt="banglow" />
+                                    <Card.ImgOverlay>
+                                        <Card.Title><h3>Sight Seeing</h3></Card.Title>
+                                    </Card.ImgOverlay>
+                            </Card>
+                        </Col>
+                    </Row>
+                     <hr/>
+                </Container>
+                <div className="head">Scuba Diving</div>
+                <Container>
+                    <hr/>    
+                   <Row>
+                       <Col md={12}>
+                            <Card className="bg-dark">
+                                <Card.Img src={scuba} alt="scuba" className="scuba-img"/>
+                                    <Card.ImgOverlay>
+                                        <Card.Text>
+                                            <div className="Scuba-Text">
+                                        <h5>- TRAINING ( Swimmer & Non Swimmer )</h5>
+                                        <h5>- DIVING WITH GUIDE</h5>
+                                        <h5>- DIVING PHOTO & VIDEO</h5>
+                                        <h5>- FOOD & BEVERAGES INCLUDED</h5>
+                                        <h5>@ RS 3500 /- *** Per Person</h5>
+                                            </div>
+                                        </Card.Text>
+                                    </Card.ImgOverlay>
+                            </Card>
+                       </Col>
+                      </Row>
+                     <hr/>
+                </Container>
         </React.Fragment>
         );    
 }
