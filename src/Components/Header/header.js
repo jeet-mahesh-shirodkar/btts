7f1b0693d89b9f0195ecd406e629ed86678b4412
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2020-07-05 14:05:32 +0530

    Nav Link

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './header.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return(
    <div className="myNav">
     <div className="Nav">
       <ul>
-        <li>Home</li>
-        <li>Vehicle Rental</li>
-        <li>Activities</li>
-        <li>About</li>
+        <li><NavLink exact activeClassName="nav-active" to="/">Home</NavLink></li>
+        <li><NavLink activeClassName="nav-active" to="/vehicle">Vehicle Rental</NavLink></li>
+        <li><NavLink activeClassName="nav-active" to="/activities">Activities</NavLink></li>
+        <li><NavLink activeClassName="nav-active" to="/contact">Contact</NavLink></li>
       </ul>
     </div>
   </div>
