8858ac7bb5424af2019a7505c9e192d3cee53148
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2020-07-15 01:35:20 +0530

    Form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [msg,setMsg] = useState("");
+    const [phone,setPhone] = useState("");
+
+    const handleForm = (event) => {
+        event.preventDefault();
+        alert(name,email,msg,phone);
+    }
     return(
     <React.Fragment>
-            <div className="head">Contact Us 📧</div>
+            <div className="head">Contact <span role="img" aria-label="email-emoji">📧</span></div>
         <div className="container contact_div">
             <div className="row">
                 <div className="col-md-6 col-10 mx-auto">
-                <form>    
+                <form onSubmit={handleForm}>    
                 <div class="mb-3">
-                    <label for="exampleFormControlInput1" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="validationCustom01" placeholder="" required/>
+                    <label className="form-label">Name</label>
+                    <input onChange={(e)=>{setName(e.target.value)}} value={name} type="text" className="form-control" required/>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleFormControlInput1" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="" required/>
+                    <label className="form-label">Phone</label>
+                    <input onChange={(e)=>{setPhone(e.target.value)}} value={phone} type="number" className="form-control" required/>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleFormControlTextarea1" className="form-label">Suggestion</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <label className="form-label">Email</label>
+                    <input onChange={(e)=>{setEmail(e.target.value)}} value={email} type="email" className="form-control" required/>
+                </div>
+                <div class="mb-3">
+                    <label className="form-label">Enquire</label>
+                    <textarea onChange={(e)=>{setMsg(e.target.value)}} value={msg} className="form-control" rows="3"></textarea>
                 </div>
                 <div class="mb-3">
                    <button class="btn btn-outline-primary" type="submit">Contact</button>
