9b0e135a43e092b24af474f9563b6411a9e4d198
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2021-04-26 16:42:58 +0530

    Updates

---
+++ precache-manifest.22fc0ac4066f3181feb159ec241f9975.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61e8f76985bf5fefbc80b321445728e",
+    "url": "/btts/index.html"
+  },
+  {
+    "revision": "64fdc860382bcc723b0d",
+    "url": "/btts/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c73cb78518d18b5f63a9",
+    "url": "/btts/static/css/main.0a432362.chunk.css"
+  },
+  {
+    "revision": "64fdc860382bcc723b0d",
+    "url": "/btts/static/js/2.5a616ff8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/btts/static/js/2.5a616ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73cb78518d18b5f63a9",
+    "url": "/btts/static/js/main.bcc8300c.chunk.js"
+  },
+  {
+    "revision": "fae9e4383b1e9770fa8a",
+    "url": "/btts/static/js/runtime-main.0f530f2a.js"
+  },
+  {
+    "revision": "389bb9af4719069bf72e9b0fe66bb8f9",
+    "url": "/btts/static/media/Banglow.389bb9af.jpg"
+  },
+  {
+    "revision": "64e108c5d8d54201d4ffa2a6ff95254c",
+    "url": "/btts/static/media/Beach-min.64e108c5.png"
+  },
+  {
+    "revision": "34778a2ed964dd0c03bdc7e429628bfd",
+    "url": "/btts/static/media/Bullet_350.34778a2e.jpg"
+  },
+  {
+    "revision": "340e678cb5395005ed16c40d6d4be448",
+    "url": "/btts/static/media/Swift.340e678c.jpg"
+  },
+  {
+    "revision": "00abd8f0a84e11b54f26d60ad6da7828",
+    "url": "/btts/static/media/bananagBoat.00abd8f0.jpg"
+  },
+  {
+    "revision": "84a424d14877eb883641ba0294da4783",
+    "url": "/btts/static/media/bumperBoat.84a424d1.jpg"
+  },
+  {
+    "revision": "7e0cb8d01fbcf68038b4b14b7ea3e50e",
+    "url": "/btts/static/media/cruise.7e0cb8d0.jpg"
+  },
+  {
+    "revision": "42726bbffa8ec8717a030906b7055181",
+    "url": "/btts/static/media/dolphin.42726bbf.jpg"
+  },
+  {
+    "revision": "f0208a4ddc76dd65ad7b1d2fb286516b",
+    "url": "/btts/static/media/jetSkiing.f0208a4d.jpg"
+  },
+  {
+    "revision": "559341ae40e18560de4f0a8378ebfc91",
+    "url": "/btts/static/media/parasailing1.559341ae.jpg"
+  },
+  {
+    "revision": "d65bec349460e7be671a2f9fe5395162",
+    "url": "/btts/static/media/scuba.d65bec34.jpg"
+  },
+  {
+    "revision": "dc8d252f5f3c790794afda69d5ebb0da",
+    "url": "/btts/static/media/snorkel-hierro.dc8d252f.jpg"
+  },
+  {
+    "revision": "2835c46695284a3b7d5c046bb6f95236",
+    "url": "/btts/static/media/sunlight.2835c466.png"
+  },
+  {
+    "revision": "fa5cab03136cea9317a74675b6f0625b",
+    "url": "/btts/static/media/waterSport1.fa5cab03.jpg"
+  }
+]);
